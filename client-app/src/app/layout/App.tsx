38ddfb00c5fda27b19cfb64d101472d112deb083
RCWall <rachel.wall@amdigital.co.uk>
2023-11-16 18:28:14 +0000

    Unit 53 Complete

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,14 @@
 
-import { Fragment, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Header } from 'semantic-ui-react';
-import { List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
@@ -16,13 +16,28 @@ function App() {
       setActivities(response.data)
     })
   }, [])
+  
+  //this function will be passed down to the ActivityDashboard -> ActivityList component and it will be called when the user clicks on the View button
+  //the function will take in the id of the activity that was clicked on and will use it to find the activity in the activities array
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id))
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
 
 
   return (
   <>  
    <NavBar />
    <Container style = {{ marginTop: '7em' }}>
-     <ActivityDashboard activities={activities} />
+     <ActivityDashboard
+      activities={activities} 
+      selectedActivity={selectedActivity}
+      selectActivity={handleSelectActivity}
+      cancelSelectActivity={handleCancelSelectActivity}
+     />
    </Container>
   </> 
   )
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -5,10 +5,11 @@ import { Segment, Item, Button, Label } from 'semantic-ui-react';
 // 2. Create the Props interface
 interface Props {
     activities: Activity[];
+    selectActivity: (id: string) => void;
 }
 
 // 3. Create the ActivityList component
-export default function ActivityList({activities}: Props) {
+export default function ActivityList({activities, selectActivity}: Props) {
     return (
         <Segment>
             <Item.Group divided>
@@ -22,7 +23,7 @@ export default function ActivityList({activities}: Props) {
                                <div>{activity.city}, {activity.venue}</div>
                            </Item.Description>
                             <Item.Extra>
-                                <Button floated='right' content='View' color='blue' />
+                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
