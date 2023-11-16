6c0547725aa56cb6f65a7614f0b4c71982478edb
RCWall <rachel.wall@amdigital.co.uk>
2023-11-16 17:43:37 +0000

    Section 51

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,34 @@
+import React from 'react' ;
+import { Activity } from '../../../app/models/activity';    // 1. Import the Activity interface
+import { Segment, Item, Button, Label } from 'semantic-ui-react';
+
+// 2. Create the Props interface
+interface Props {
+    activities: Activity[];
+}
+
+// 3. Create the ActivityList component
+export default function ActivityList({activities}: Props) {
+    return (
+        <Segment>
+            <Item.Group divided>
+                {activities.map(activity => (
+                    <Item  key={activity.id}>
+                        <Item.Content>
+                           <Item.Header as='a'>{activity.title}</Item.Header>
+                           <Item.Meta>{activity.date}</Item.Meta>
+                           <Item.Description>
+                               <div>{activity.description}</div>
+                               <div>{activity.city}, {activity.venue}</div>
+                           </Item.Description>
+                            <Item.Extra>
+                                <Button floated='right' content='View' color='blue' />
+                                <Label basic content={activity.category} />
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>       
+        </Segment>
+    )} 
+
