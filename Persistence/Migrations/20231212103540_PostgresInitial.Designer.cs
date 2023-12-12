abc807bbbd77fb42854dc15196904a68604f5022
RCWall <rachel.wall@amdigital.co.uk>
2023-12-12 11:04:29 +0000

    adding private routes

--- Persistence/Migrations/20231212103540_PostgresInitial.Designer.cs
+++ Persistence/Migrations/20231212103540_PostgresInitial.Designer.cs
@@ -12,7 +12,7 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20231211174947_PostgresInitial")]
+    [Migration("20231212103540_PostgresInitial")]
     partial class PostgresInitial
     {
         /// <inheritdoc />
--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -8,19 +8,20 @@ import TestErrors from "../../features/errors/TestError";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
 import { Navigate } from "react-router";
-import LoginForm from "../../features/users/LoginForm";
+import RequireAuth from "./RequireAuth";
 
 export const routes: RouteObject[] = [
     {
         path: "/",
         element: <App />,
         children: [
-            {path: 'activities', element: <ActivityDashboard />},
-            {path: 'activities/:id', element: <ActivityDetails />},
-            {path: 'createActivity', element: <ActivityForm  key='create'/>},
-            {path: 'manage/:id', element: <ActivityForm key='manage'/>},
-            {path: 'login', element: <LoginForm/>},
-            {path: 'errors', element: <TestErrors/>},
+            {element:<RequireAuth />, children: [
+                {path: 'activities', element: <ActivityDashboard />},
+                {path: 'activities/:id', element: <ActivityDetails />},
+                {path: 'createActivity', element: <ActivityForm  key='create'/>},
+                {path: 'manage/:id', element: <ActivityForm key='manage'/>},
+                {path: 'errors', element: <TestErrors/>},
+            ]},
             {path: 'not-found', element: <NotFound/>},
             {path: '*', element: <Navigate replace to='/not-found'/>},
             {path: 'server-error', element: <ServerError/>},
